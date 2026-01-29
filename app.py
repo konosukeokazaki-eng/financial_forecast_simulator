491ff1e241f0b00b2c7a758a75f7cd6aaa525c92
konosukeokazaki-eng <konosuke.okazaki@cmind-co.jp>
2026-01-29 17:41:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -851,7 +851,7 @@ if 'selected_period_id' in st.session_state and st.session_state.selected_period
             # フォーマット
             formatted_df = display_df.style\
                 .format(lambda x: f"¥{int(x):,}" if isinstance(x, (int, float)) else x)\
-                .apply(lambda row: ['background-color: #f8f9fa; font-weight: bold' if row['タイプ'] == '要約' else '' for _ in row], axis=1)
+                .apply(lambda row: ['background-color: #0b2036; font-weight: bold' if row['タイプ'] == '要約' else '' for _ in row], axis=1)
             
             st.dataframe(formatted_df, width="stretch", height=700)
             
