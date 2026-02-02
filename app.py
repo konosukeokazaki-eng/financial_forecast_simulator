b9a22686c2dc763e889ef3c91587c98c29b90b03
konosukeokazaki-eng <konosuke.okazaki@cmind-co.jp>
2026-02-02 13:25:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2483,7 +2483,7 @@ if 'selected_period_id' in st.session_state and st.session_state.selected_period
                                 "成長率(%)": "{:.1f}%"
                             })\
                             .applymap(
-                                lambda x: 'background-color: #7efc9d' if isinstance(x, (int, float)) and x > 0 else 
+                                lambda x: 'background-color: #6eff91' if isinstance(x, (int, float)) and x > 0 else 
                                           ('background-color: #ff96a0' if isinstance(x, (int, float)) and x < 0 else ''),
                                 subset=['増減額', '成長率(%)']
                             )
